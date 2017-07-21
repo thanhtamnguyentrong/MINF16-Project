390015e5a7c3bc716b49599c497c7c36b552786e
thanhtamnguyentrong <thanhtam@bhdvn.com>
2017-07-21 16:03:22 +0700

    fix error

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -10,31 +10,42 @@ var async = require('async');
 var db = require('../db')
 
 function addRestaurant(body,callback){
-	console.log(body);
 	var collection = db.get().collection('Restaurant');
-	var restaurantIndex = countRestaurant() + 1 ;
-	body._id = restaurantIndex;
-	collection.insertOne(body).then(function(result){
-		collection.find().toArray(function(err,docs){
-			if(err){
-				console.log(err);
-				res.status(401).json(err);
-			}
-	    	else {
-	    		console.log(docs);
-	    		callback(docs);
-	    	}
-		})
+	countRestaurant(function(err, count){
+		if(err)
+			callback(err,null);
+		else{
+			var id = count+1;
+			body._id = id;
+			collection.insertOne(body,function(err,result){
+				if(err){
+					console.log(err);
+					callback(err,null);
+				} else{
+					callback(null,result.insertedId)
+				}
+				// collection.find().toArray(function(err,docs){
+				// 	if(err){
+				// 		console.log(err);
+				// 		callback(err,null);
+				// 	}
+			 //    	else {
+			 //    		callback(null,docs);
+			 //    	}
+				// })
+			})
+		}
 	})
+	
 }
 
-function countRestaurant(){
+function countRestaurant(callback){
 	var collection = db.get().collection('Restaurant');
 	collection.find().count(function(e,count){
-		if(err)
-			return -1;
+		if(e)
+			callback(e,-1);
 		else
-			return count;
+			callback(null,count);
 	});
 }
 //Routes
@@ -48,7 +59,7 @@ router.get('/', function(req,res){
 		}
     	else {
     		//console.log(docs);
-    		res.status(200).json(docs);
+    		res.json(docs);
     	}
   	})
 });
@@ -60,15 +71,27 @@ router.post('/', cache('seconds', 0), function(req, res) {
     // var productId = req.params.id;
     // var episodeId = req.body.episode_id;
     // getStreams(clientId, productId, episodeId, res);
-    addRestaurant(body,function(docs){
-    	res.json(docs);
+    addRestaurant(body,function(err,insertedId){
+    	if(err)
+    		//res.status(400).json(err);
+    		console.log(err);
+    	else
+    		res.json({status: "success",
+    					restaurant_id: insertedId});
     });
 
 });
 
+//get restaurant count
 router.get('/count', function(req,res){
-	res.json(countRestaurant());
+	countRestaurant(function(err, count){
+		if(err)
+			res.status(400).json(err);
+		else
+			res.json(count);
+	})
 	
 });
+
 //Return router
 module.exports = router;
